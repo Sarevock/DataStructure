f5e43398efe767bde6cd0f3ed5a0495433770edd
Sarevok <Mimmius89@outlook.it>
2021-04-17 16:07:01 +0200

    Nuovo

--- Game/Four.c
+++ Game/Four.c
@@ -3,20 +3,24 @@
 #include<string.h>
 #include<stdbool.h>
 
-//Connect 4 Pitch
+//Connect 4 Board
 struct pitch{	
 	int a[6][7];
 	int col[7];
 };
 
 //Save Games
+//Max Save Slots
 #define MAX 10
+//Max Moves
+#define MAX2 84
+
 //Singolar Record
 struct rec{
 	int start;
-	int re[84];
+	int re[MAX2];
 };
-//Max 10 Saved games
+//Memory Card
 struct saveGames{
 	int salvataggio;
 	struct rec save[MAX];
@@ -30,12 +34,22 @@ void insert(struct pitch* , int , int );
 bool winner (struct pitch* );
 
 //SaveGames
-void init_Save(struct saveGames* );
-void record(struct saveGames* , int , bool );
 
-//Test
-void vediamo(struct pitch* );
-void print(struct saveGames* , int );
+//Singola Save
+void singolar_init(struct rec* );
+void singolar_add(struct rec* , int );
+int *singolar_do(struct rec* , int );
+int *singolar_undo(struct rec* , int );
+void printami(struct rec* );
+
+//Memory Card
+void init_memory(struct saveGames* );
+void record(struct saveGames* ,  struct rec );
+bool isEmpty (struct saveGames * );
+void selection(struct saveGames* );
+void exchange(struct rec* , struct saveGames , int );
+void vedere(struct saveGames* );
+
 
 int main(){
 
@@ -46,10 +60,15 @@ int main(){
 	int position;
 	bool win = false;
 	//SaveGame
-	int move = 0;
-	int save;
-	struct saveGames salvataggio;
-	init_Save(&salvataggio);
+	int save = 0;	
+	int move = -1;
+	struct saveGames sa;
+	struct rec ve;
+	bool saveGa = false;
+	//init memory
+	init_memory(&sa);
+	int *i = NULL ;
+	int last;
 
 	//Menu
 	int choice = 0;
@@ -68,24 +87,39 @@ int main(){
 		switch (choice)
 		{
 		case 1:
-
 			win = false;
 			clrscr();
 			init(&campo);
-			record(&salvataggio, NULL, true);
-			save = salvataggio.salvataggio;
+			singolar_init(&ve);
 			while (win == false){
 
 				clrscr();				
 				printBoard(&campo);
 
 				if (winner(&campo) == true){
-					sleep(1);
 					player = 1;
-					choice = 4;
-					break;
+					printf("\n\nPress 1 : Yes");
+					printf("\nPress 2 : No");
+					printf("\nSave the Game?: ", choice);					
+					scanf_s("%d", &choice);
+					if (choice == 1){
+						record(&sa, ve);
+						printf("\nSaved!");
+						printf("\nRedirecting to Main Menu!");
+						sleep(1);
+						break;
+					}
+					else if (choice == 2){
+						printf("\nRedirecting to Main Menu!");
+						sleep(1);
+						break;
+					}
+					else{
+						printf("\nRedirecting to Main Menu!");
+						sleep(1);
+						break;
+					}					
 				}	
-				
 				printf("\nPress  0 : Reset the Game");
 				printf("\nPress 1-7: Select a Column");
 				printf("\n\nPlayer %d Enter Your Choice: ", player);
@@ -94,12 +128,13 @@ int main(){
 				if(position == 0){
 					player = 1;
 					init(&campo);
+					singolar_init(&ve);
 				}
 				else if (position <= 1 || position <=7){
 					position--;
 					if(campo.col[position] < 6){
 						insert(&campo, player, position);
-						record(&salvataggio, position,  false);
+						singolar_add(&ve, position);
 						player = change(player);
 					}
 					else{
@@ -114,16 +149,99 @@ int main(){
 			}	
 			break;
 		case 2:
-			printf("\nYOU SELECTED OPTION 2 %c", 2);
+			saveGa = false;
+			player = 1;
+			save = -1;
+			move = -1;
+			clrscr();
+			init(&campo);
+			singolar_init(&ve);
+
+			if(isEmpty(&sa) ==  true){
+				printf("No Save Data"); 
+				saveGa = true;
+			}
+			else{
+
+				printf("\nConnect 4");
+				printf("\n------------------------------\n");
+				selection(&sa);
+				printf("\n\n Enter Your Choice: ");
+				scanf_s("%d", &choice);
+				save = choice - 1;
+				//pass data
+				exchange(&ve, sa, save);
+				last = ve.start;
+				move = -1;
+				//print on screen
+				printf("\nManual Replay : 1");
+				printf("\nAuto Replay : 2");
+				printf("\n\nSelect 1 or 2?: ");					
+				scanf_s("%d", &choice);
+			}	
+
+			while(saveGa == false){
+				
+				clrscr();				
+				printBoard(&campo);	
+
+				if (choice == 1){
+
+					printf("\n\t 1. Do");
+					printf("\n\t 2. Undo");
+					printf("\n\t 3. Modify");
+					printf("\n\t 4. Exit");
+
+					printf("\n\n Enter Your Choice: ");
+					scanf_s("%d", &position);
+
+					if (position == 1){
+						move++;
+						i = singolar_do (&ve, move);
+						if(i){							
+							printf (" Move Restored : %d\n", *i); 							
+							insert(&campo, player, *i);
+							player = change(player);												
+							sleep(1);
+						}
+					}
+					else if(position == 2){
+						i = singolar_undo (&ve , move);
+						if(i){
+							printf (" Move Removed : %d\n", *i); 
+							out(&campo,  *i);
+							player = change(player);
+							move--;
+						}
+						sleep(1);
+					}
+					else if (position == 3){
+
+					}
+					else if (position == 4){
+						choice = 0;
+						break;
+					}
+				}
+				else if (choice == 2){
+					if (winner(&campo) == true){
+						break;
+					}
+					if(move == -1){
+						move = 0;
+					}
+					position = ve.re[move];
+					printf("%d", ve.re[move]);
+					insert(&campo, player, position);
+					player = change(player);
+					move++;	
+					sleep(1);								
+				}				
+			}
 			break;
 		case 3:
 			printf("\nYOU SELECTED OPTION 3 %c", 3);
 			exit(0);
-		case 4:
-			player = 1;
-			win = false;
-			choice = 0;
-			break;
 		otherwise:
 			printf("\nINVALID SELECTION...Please try again");
 		}
@@ -133,8 +251,25 @@ int main(){
 	return 0;
 }
 
-//Board
 
+//Game
+
+//Change player
+int change(int change){
+	if(change == 1){
+		change = 2;
+	}
+	else if (change == 2){
+		change =1;
+	}
+	return change;
+}
+//Screen Clear
+void clrscr(){
+    system("@cls||clear");
+}
+
+//Board
 //Initialization
 void init(struct pitch* campo){
 	for (int i = 0; i< 6; i++){
@@ -168,17 +303,6 @@ void printBoard(struct pitch* campo){
 	}
 	printf("\n");
 }
-//Change player
-int change(int change){
-	
-	if(change == 1){
-		change = 2;
-	}
-	else if (change == 2){
-		change =1;
-	}
-	return change;
-}
 //Insert player token
 void insert(struct pitch* campo, int player, int pos){
 	if (player == 1){
@@ -191,8 +315,10 @@ void insert(struct pitch* campo, int player, int pos){
 	}	
 }
 //Remove move
-void out(struct pitch* campo, int player, int pos){
-	
+void out(struct pitch* campo, int pos){
+	campo->col[pos] = campo->col[pos] -1;
+	int sapere = campo->col[pos];
+	campo->a[sapere][pos] = 0;
 }
 //Algoriths to find a winner
 bool winner (struct pitch* campo){
@@ -345,53 +471,107 @@ bool winner (struct pitch* campo){
 		}
 	}
 }
-//Screen Clear
-void clrscr(){
-    system("@cls||clear");
-}
+
 
 //SaveGame
-
 //Init
-void init_Save(struct saveGames* sa){
+void init_memory(struct saveGames* sa){
 	sa->salvataggio = -1;
 }
-
 //Add
-void record(struct saveGames* sa, int pos, bool new){
+void record(struct saveGames* sa,  struct rec ve){
 
 	if(sa->salvataggio == MAX -1){
         printf("Save Game is full\n");
         return;    
     }
 
-	if (new == true){
-		sa->salvataggio++;
-		sa->save->start = 0;
-	}
-	else{
-		sa->save[sa->salvataggio].re[sa->save->start] = pos;
-    	sa->save->start++;
-	}
+	sa->salvataggio++;
+	sa->save[sa->salvataggio] = ve;
 }
 //Remove
-void drocer(){
+void drocer(struct saveGames* sa, int ve){
 
 }
-
-//Test
-void vediamo(struct pitch* campo){
-	//test
-	campo->a[0][0] = 1;
-	campo->a[1][1] = 1;
-	campo->a[2][2] = 1;
-	campo->a[3][3] = 1;
+//Test Print
+void vedere(struct saveGames* sa){
+	if(sa->salvataggio < 0){
+		printf("Nothing Here!");
+	}
+	else{
+		for (int i = 0; i <= sa->salvataggio; i++){
+			for (int j = 0; j <= sa->save[i].start; j++){
+				printf("%d", sa->save[i].re[j]);
+			}
+			printf("\n");
+		}
+		printf("%d", sa->save->start);
+	}	
+}
+//Check Data
+bool isEmpty (struct saveGames* sa){
+	if (sa->salvataggio == -1){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+//Create SaveGames Selection
+void selection(struct saveGames* sa){
+	int save;
+	for (int i = 0; i <= sa->salvataggio; i++){
+		save = i + 1;
+		printf("Replay Salvataggio %d\n", save);
+	}
+}
+//Retrieve Save Game and cast it to Singolar Save
+void exchange(struct rec* ve, struct saveGames sa, int which){
+	for (int i = 0; i <= sa.save[which].start; i++){
+		singolar_add(ve, sa.save[which].re[i]);
+	}
 }
 
-//Pritn save games
-void print(struct saveGames* sa, int move){
-	
-	for(int i = 0; i < move; i++){
-		printf("%d", sa->save[0].re[i]);
+
+//Singolar Save
+void singolar_init(struct rec* sa){
+	sa->start = -1;
+}
+//Add
+void singolar_add(struct rec* sa, int pos){
+	if(sa->start == MAX2 -1){
+        printf("Save Game is full\n");
+        return;    
+    }
+	sa->start++;
+	sa->re[sa->start] = pos;
+}
+//Redo
+int *singolar_do(struct rec* sa, int move){
+	int *data;
+	if(move == sa->start){
+		printf("Last Move\n");
+		return NULL;
+	}
+
+	data = &sa->re[move];
+	return data;
+}
+//Undo
+int *singolar_undo(struct rec* sa, int move){
+	int *data;
+	if(move == -1){
+        printf("No more Undo\n");
+        return NULL;
+    }
+
+	data = &sa->re[move];
+    return data;
+}
+//Test Print
+void printami(struct rec* sa){
+	for (int i = 0; i < sa->start; i++){
+		printf("%d", sa->re[sa->start]);
+		printf("\n%d", sa->start);
 	}
 }
